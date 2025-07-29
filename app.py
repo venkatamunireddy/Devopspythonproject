8acd1517dee56600c31c9a24cf4e0dbb9c577c5a
VenkatamuniReddy Udarlla <udarllavankatamuni.reddy@mavenir.com>
2025-07-29 12:42:43 +0530

    files structure is changed in the repository.

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello from Flask in Docker!'
 
-if __name__  == '__main__':
+if __name__ == '__main__':
 
     app.run(host='0.0.0.0', port=5000) 
